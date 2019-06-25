5428d569c3c8ea6cccbfad99b97340720264f8f7
pitprok <44905554+pitprok@users.noreply.github.com>
2019-06-25 11:00:23 +0300

    Initial working demo

---
+++ src/main/java/HelloJNI.c
@@ -0,0 +1,9 @@
+#include <jni.h>        // JNI header provided by JDK
+#include <stdio.h>      // C Standard IO Header
+#include "HelloJNI.h"   // Generated
+#include "HelloWorld.h" // Original C code I want to use in Java 
+// Implementation of the native method sayHello()
+JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
+   helloworld();
+   return;
+}
